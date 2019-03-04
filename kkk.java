bd62114b488747f5ffe6af75e97a2c85e5510546
ShengdongYan <yanshengdong@bupt.edu.cn>
2019-03-04 00:19:51 +0000

    project

---
+++ kkk.java
@@ -0,0 +1,17 @@
+public class kkk {
+/**
+ * jello
+ * @author Shengdong Yan
+ * @version 2019-03-03
+ */
+private static String EJ = "sg";
+    public static void main(String[] args) {
+        System.out.println("s");
+
+
+    }
+}
+
+
+
+
